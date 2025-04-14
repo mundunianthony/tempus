9793722ac0a5902c1f673270c0bec4a8ca376e3f
munduni anthony <mundunianthony@gmail.com>
2025-04-14 23:18:21 +0300

    Add CustomButton widget for reusable button component

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size.fromHeight(50),
+        backgroundColor: Colors.deepOrange,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      onPressed: onPressed,
+      child: Text(text, style: const TextStyle(fontSize: 16)),
+    );
+  }
+}
