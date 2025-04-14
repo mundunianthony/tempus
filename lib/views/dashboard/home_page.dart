f1a4dfe716f5c0bcb1a3e822b8c08ee720678174
munduni anthony <mundunianthony@gmail.com>
2025-04-14 23:20:46 +0300

    Add HomePage to display user dashboard with logout functionality

---
+++ lib/views/dashboard/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/auth_provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tempus Dashboard'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              Provider.of<AuthProvider>(context, listen: false).signOut();
+            },
+          ),
+        ],
+      ),
+      body: const Center(child: Text('You are logged in!')),
+    );
+  }
+}
